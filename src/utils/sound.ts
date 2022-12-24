6b850ae1b288b03d01a24834066f5b0593b9814a
Woojin Lee <nijoow1127@gmail.com>
2022-12-24 17:36:36 +0900

    피아노 사운드 추가

---
+++ src/utils/sound.ts
@@ -0,0 +1,224 @@
+interface PianoSounds {
+  url: string;
+  name: string;
+  color: "W" | "B";
+  keyCode: string;
+}
+export const pianoSounds: PianoSounds[] = [
+  {
+    url: "/audio/piano/piano48.wav",
+    name: "도",
+    color: "W",
+    keyCode: "1",
+  },
+  {
+    url: "/audio/piano/piano49.wav",
+    name: "도#",
+    color: "B",
+    keyCode: "2",
+  },
+  {
+    url: "/audio/piano/piano50.wav",
+    name: "레",
+    color: "W",
+    keyCode: "3",
+  },
+  {
+    url: "/audio/piano/piano51.wav",
+    name: "레#",
+    color: "B",
+    keyCode: "4",
+  },
+  {
+    url: "/audio/piano/piano52.wav",
+    name: "미",
+    color: "W",
+    keyCode: "5",
+  },
+  {
+    url: "/audio/piano/piano53.wav",
+    name: "파",
+    color: "W",
+    keyCode: "6",
+  },
+  {
+    url: "/audio/piano/piano54.wav",
+    name: "파#",
+    color: "B",
+    keyCode: "7",
+  },
+  {
+    url: "/audio/piano/piano55.wav",
+    name: "솔",
+    color: "W",
+    keyCode: "8",
+  },
+  {
+    url: "/audio/piano/piano56.wav",
+    name: "솔#",
+    color: "B",
+    keyCode: "9",
+  },
+  {
+    url: "/audio/piano/piano57.wav",
+    name: "라",
+    color: "W",
+    keyCode: "0",
+  },
+  {
+    url: "/audio/piano/piano58.wav",
+    name: "라#",
+    color: "B",
+    keyCode: "-",
+  },
+  {
+    url: "/audio/piano/piano59.wav",
+    name: "시",
+    color: "W",
+    keyCode: "=",
+  },
+  {
+    url: "/audio/piano/piano60.wav",
+    name: "도",
+    color: "W",
+    keyCode: "q",
+  },
+  {
+    url: "/audio/piano/piano61.wav",
+    name: "도#",
+    color: "B",
+    keyCode: "w",
+  },
+  {
+    url: "/audio/piano/piano62.wav",
+    name: "레",
+    color: "W",
+    keyCode: "e",
+  },
+  {
+    url: "/audio/piano/piano63.wav",
+    name: "레#",
+    color: "B",
+    keyCode: "r",
+  },
+  {
+    url: "/audio/piano/piano64.wav",
+    name: "미",
+    color: "W",
+    keyCode: "t",
+  },
+  {
+    url: "/audio/piano/piano65.wav",
+    name: "파",
+    color: "W",
+    keyCode: "y",
+  },
+  {
+    url: "/audio/piano/piano66.wav",
+    name: "파#",
+    color: "B",
+    keyCode: "u",
+  },
+  {
+    url: "/audio/piano/piano67.wav",
+    name: "솔",
+    color: "W",
+    keyCode: "i",
+  },
+  {
+    url: "/audio/piano/piano68.wav",
+    name: "솔#",
+    color: "B",
+    keyCode: "o",
+  },
+  {
+    url: "/audio/piano/piano69.wav",
+    name: "라",
+    color: "W",
+    keyCode: "p",
+  },
+  {
+    url: "/audio/piano/piano70.wav",
+    name: "라#",
+    color: "B",
+    keyCode: "[",
+  },
+  {
+    url: "/audio/piano/piano71.wav",
+    name: "시",
+    color: "W",
+    keyCode: "]",
+  },
+  {
+    url: "/audio/piano/piano72.wav",
+    name: "도",
+    color: "W",
+    keyCode: "a",
+  },
+  {
+    url: "/audio/piano/piano73.wav",
+    name: "도#",
+    color: "B",
+    keyCode: "s",
+  },
+  {
+    url: "/audio/piano/piano74.wav",
+    name: "레",
+    color: "W",
+    keyCode: "d",
+  },
+  {
+    url: "/audio/piano/piano75.wav",
+    name: "레#",
+    color: "B",
+    keyCode: "f",
+  },
+  {
+    url: "/audio/piano/piano76.wav",
+    name: "미",
+    color: "W",
+    keyCode: "g",
+  },
+  {
+    url: "/audio/piano/piano77.wav",
+    name: "파",
+    color: "W",
+    keyCode: "h",
+  },
+  {
+    url: "/audio/piano/piano78.wav",
+    name: "파#",
+    color: "B",
+    keyCode: "j",
+  },
+  {
+    url: "/audio/piano/piano79.wav",
+    name: "솔",
+    color: "W",
+    keyCode: "k",
+  },
+  {
+    url: "/audio/piano/piano80.wav",
+    name: "솔#",
+    color: "B",
+    keyCode: "l",
+  },
+  {
+    url: "/audio/piano/piano81.wav",
+    name: "라",
+    color: "W",
+    keyCode: ";",
+  },
+  {
+    url: "/audio/piano/piano82.wav",
+    name: "라#",
+    color: "B",
+    keyCode: "'",
+  },
+  {
+    url: "/audio/piano/piano83.wav",
+    name: "시",
+    color: "W",
+    keyCode: "Enter",
+  },
+];
