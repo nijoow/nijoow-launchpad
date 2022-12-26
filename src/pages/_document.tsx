bba8f454cff7731a0b5f5b33f2b8644310d7a8a9
Woojin Lee <nijoow1127@gmail.com>
2022-12-27 00:45:39 +0900

    폰트수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,10 @@ const _document = () => {
           href="https://fonts.googleapis.com/css2?family=Gugi&family=Jua&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
