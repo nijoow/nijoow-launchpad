d5129cbca3484852d8c72bcd3f1fbc3bc2e2836a
Woojin Lee <nijoow1127@gmail.com>
2023-01-11 01:33:16 +0900

    spotify 삭제

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,52 +1,12 @@
-import React, { useEffect, useState } from "react";
-import {
-  IoPlayCircle,
-  IoPlaySkipBack,
-  IoPlaySkipForward,
-  IoRepeat,
-  IoShuffle,
-  IoPauseCircle,
-} from "react-icons/io5";
-const Footer = () => {
-  const [percent, setPercent] = useState(0);
+import React from "react";
 
+const Footer = () => {
   return (
-    <div className="grid h-24 grid-cols-10 bg-zinc-700 text-zinc-400">
-      {/* <div className="flex items-center justify-center h-full mx-auto max-w-7xl">
+    <div className="flex h-24 bg-zinc-700 text-zinc-400">
+      <div className="flex items-center justify-center h-full mx-auto max-w-7xl">
         {" "}
         &copy; {new Date().getFullYear()} Lee Woo Jin. All Rights Reserved.
-      </div> */}
-      <div className="flex items-center col-span-3 gap-4">
-        <div className="w-24 h-24 bg-white"></div>
-        <div className="flex flex-col">
-          <span className="font-semibold">title</span>
-          <span className="text-sm">singer</span>
-        </div>
       </div>
-      <div className="flex flex-col items-center justify-center col-span-4 gap-2">
-        <div className="flex items-center gap-4">
-          {" "}
-          <IoRepeat size={24} />
-          <IoPlaySkipBack size={24} />
-          <IoPlayCircle size={42} onClick={() => {}} />
-          <IoPauseCircle size={42} onClick={() => {}} />
-          <IoPlaySkipForward size={24} />
-          <IoShuffle size={24} />
-        </div>
-        <div className="flex items-center w-full gap-1 text-xs">
-          <span>0:01</span>
-          <div className="flex justify-start flex-auto h-1 rounded-full bg-zinc-500">
-            <div
-              className={`flex bg-zinc-50 h-1 transition-all items-center`}
-              style={{ width: `${percent}%` }}
-            >
-              <div className="translate-x-1.5 w-3 h-3 ml-auto bg-white rounded-full "></div>
-            </div>
-          </div>
-          <span>4:30</span>
-        </div>
-      </div>
-      <div className="flex items-center col-span-3"></div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,11 @@ import useSound from "use-sound";
 import KeyPad from "../components/launchpad/KeyPad";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { ACCESS_URL, getTokenFromResponse } from "../utils/spotify";
-import SpotifyWebApi from "spotify-web-api-js";
-
-const spotify = new SpotifyWebApi();
 
 export async function getServerSideProps() {
-  console.log("!");
   return { props: {} };
 }
 const Home: NextPage = () => {
-  const [token, setToken] = useState();
-
-  useEffect(() => {
-    const hash = getTokenFromResponse();
-    window.location.hash = "";
-    let _token = hash.access_token;
-    if (_token) {
-      setToken(hash.access_token);
-      localStorage.setItem("spotify_token", _token);
-      spotify.setAccessToken(_token);
-    }
-  }, []);
-  console.log(token);
   return (
     <div className="grid items-center justify-center w-full h-full grid-cols-12 overflow-hidden">
       <div className="flex flex-col items-center w-full h-full col-span-2 gap-4 p-12 bg-zinc-700">
@@ -36,16 +18,7 @@ const Home: NextPage = () => {
       </div>{" "}
       <div
         className={`flex relative items-center justify-center col-span-10 gap-4 mx-auto w-fit transition-all `}
-      >
-        {!token && (
-          <Link
-            href={ACCESS_URL}
-            className="px-4 py-2.5 text-xl bg-green-500 rounded-md"
-          >
-            Login with <span className="text-white">Spotify</span>
-          </Link>
-        )}
-      </div>
+      ></div>
     </div>
   );
 };
